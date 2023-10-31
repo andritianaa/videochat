967ccbd3f6d645084e4ef3e115fbc26a499ffaad
andritianaa <andritianasteve@gmail.com>
2023-10-31 11:02:55 +0300

    split script and update button's design

--- public/script/call.js
+++ public/script/call.js
@@ -1,80 +1,55 @@
-
-const copyText = document.getElementById('copyText');
-copyText.addEventListener('click', (e) => {
-    let currentURL = window.location.href;
-    currentURL = currentURL.replace(/(^\w+:|^)\/\//, '');
-    navigator.clipboard.writeText(currentURL)
-        .then(() => {
-            console.log('URL copied to clipboard: ', currentURL);
-            alert('URL copied to clipboard: ' + currentURL);
-        })
-        .catch(err => {
-            console.error('Error copying URL to clipboard: ', err);
-        });
-});
-
-const socket = io('https://video.teratany.org');
-const localVideo = document.getElementById('localVideo');
-const remoteVideo = document.getElementById('remoteVideo');
-const statusDiv = document.getElementById('status');
-let peer;
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const callName = urlParams.get('call');
-console.log(callName);
+const socket = io('https://video.teratany.org')
+const localVideo = document.getElementById('localVideo')
+const remoteVideo = document.getElementById('remoteVideo')
+const statusDiv = document.getElementById('status')
+const callName = new URLSearchParams(window.location.search).get('call')
 if (!callName) window.location.href = "index.html"
 
+let peer
+let model
 // Initialiser le modèle BodyPix
-let model;
 const bodyPixConfig = {
     architecture: 'MobileNetV1',
     outputStride: 16,
     multiplier: 1,
     quantBytes: 2
-};
+}
 
-const loadBodyPixModel = async () => {
-    console.log("loadBodyPix");
-    model = await bodyPix.load(bodyPixConfig);
-};
+const loadBodyPixModel = async () => model = await bodyPix.load(bodyPixConfig)
 
 // Flouter l'arrière-plan
 const blurBackground = async (video, canvas) => {
-    const segmentation = await model.segmentPerson(video);
-    const backgroundBlurAmount = 6;
-    bodyPix.drawBokehEffect(canvas, video, segmentation, backgroundBlurAmount, 5);
-};
+    const segmentation = await model.segmentPerson(video)
+    const backgroundBlurAmount = 6
+    bodyPix.drawBokehEffect(canvas, video, segmentation, backgroundBlurAmount, 5)
+}
 
 
 navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(stream => {
-        localVideo.srcObject = stream;
-        socket.emit('join', callName);
+        localVideo.srcObject = stream
+        socket.emit('join', callName)
         socket.on('other-user', otherUserId => {
-            peer = new SimplePeer({ initiator: true, trickle: false, stream });
-            peer.on('signal', signal => socket.emit('offer', signal));
+            peer = new SimplePeer({ initiator: true, trickle: false, stream })
+            peer.on('signal', signal => socket.emit('offer', signal))
             peer.on('stream', stream => {
-                remoteVideo.srcObject = stream;
-                statusDiv.textContent = '';
-            });
-            socket.on('answer', answer => peer.signal(answer));
-        });
+                remoteVideo.srcObject = stream
+                statusDiv.textContent = ''
+            })
+            socket.on('answer', answer => peer.signal(answer))
+        })
 
         socket.on('offer', offer => {
-            peer = new SimplePeer({ initiator: false, trickle: false, stream });
-            peer.on('signal', signal => socket.emit('answer', signal));
+            peer = new SimplePeer({ initiator: false, trickle: false, stream })
+            peer.on('signal', signal => socket.emit('answer', signal))
             peer.on('stream', stream => {
-                remoteVideo.srcObject = stream;
-                statusDiv.textContent = '';
-            });
-            peer.signal(offer);
-        });
+                remoteVideo.srcObject = stream
+                statusDiv.textContent = ''
+            })
+            peer.signal(offer)
+        })
 
-        socket.on('user-disconnected', () => {
-            statusDiv.textContent = 'Your friend has left the call.';
-        });
-        loadBodyPixModel();
+        socket.on('user-disconnected', () => statusDiv.textContent = 'Your friend has left the call.')
+        loadBodyPixModel()
     })
-    .catch(error => { console.error('Error accessing media devices: ', error) });
-
-
+    .catch(error => { console.error('Error accessing media devices: ', error) })
